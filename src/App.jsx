0689b98c1697171967a7e4acebf3a242e11777fc
MD ABU TALHA-11 <mdabutalha01742@gmail.com>
2026-03-11 15:19:45 +0600

    Fix: Add basename to Router for GitHub Pages subdirectory

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Blog">
       <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
